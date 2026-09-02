5b1ce7262bc623bd006a166681744c1c68ef42fe
RajAgr-awal <RajAgr-awal@users.noreply.github.com>
2026-09-02 19:05:17 +0000

    deploy: a2e119677f50cc8db83e77d74f9781776d820971

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@ const IMAGE_CACHE = `ww-img-${CACHE_VERSION}`;
 const MAX_IMAGES = 120;
 
 // Same-origin assets that make the app usable with no network at all.
-const SHELL_ASSETS = ['./', './index.html', './manifest.webmanifest', './offline.html', "./assets/index-DZtRjBP7.js", "./assets/index-DZX7G28k.css"];
+const SHELL_ASSETS = ['./', './index.html', './manifest.webmanifest', './offline.html', "./assets/index-DlYneQnM.js", "./assets/index-DZX7G28k.css"];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
